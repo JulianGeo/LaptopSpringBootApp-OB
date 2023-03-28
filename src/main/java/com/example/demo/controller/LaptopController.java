24e3117aba816326f851ea996c7d3a2b892c0a99
Julian Rojas <julianrojasgeo@gmail.com>
2023-03-28 10:02:58 -0500

    Controladores principales

--- src/main/java/com/example/demo/controller/LaptopController.java
+++ src/main/java/com/example/demo/controller/LaptopController.java
@@ -2,12 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Laptop;
 import com.example.demo.service.LaptopService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.lang.annotation.Retention;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class LaptopController {
@@ -19,12 +19,36 @@ public class LaptopController {
     }
 
     @GetMapping("laptops")
-    public List<Laptop> getLaptops(){
-        return service.getLaptops();
+    public ResponseEntity<List<Laptop>> getLaptops(){
+        return ResponseEntity.ok().body(service.getLaptops());
     }
 
+    @GetMapping("laptops/{id}")
+    public ResponseEntity<Optional<Laptop>> getLaptopById(@PathVariable String id){
+        return ResponseEntity.ok().body(service.getLaptopByID(id));
+    }
     @PostMapping("laptops")
-    public Laptop saveLaptop(@RequestBody Laptop laptop){
-        return service.saveLaptop(laptop);
+    public ResponseEntity<Laptop> saveLaptop(@RequestBody Laptop laptop){
+        if (laptop.getLaptopID() != null){return  ResponseEntity.badRequest().build();}
+        return ResponseEntity.ok().body(service.saveLaptop(laptop));
+    }
+
+    @PutMapping("laptops/{id}")
+    public ResponseEntity<Laptop> updateLaptop(@RequestBody Laptop laptop){
+        if (laptop.getLaptopID() == null){return  ResponseEntity.badRequest().build();}
+        return ResponseEntity.ok().body(service.updateLaptop(laptop));
+    }
+
+    @DeleteMapping("laptops")
+    public ResponseEntity<String> deleteLaptops(){
+        service.deleteAll();
+        return ResponseEntity.ok().body("All laptops were deleted");
+    }
+
+    @DeleteMapping("laptops/{id}")
+    public ResponseEntity<String> deleteLaptops(@PathVariable String id){
+        service.deleteByID(id);
+        return ResponseEntity.ok().body("Laptop with ID: "+id+", was deleted");
+
     }
 }
--- src/main/java/com/example/demo/service/LaptopService.java
+++ src/main/java/com/example/demo/service/LaptopService.java
@@ -5,6 +5,7 @@ import com.example.demo.repository.LaptopRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,8 +20,25 @@ public class LaptopService {
     public List<Laptop> getLaptops() {
         return laptopRepository.findAll();
     }
+    public Optional<Laptop> getLaptopByID(String id) {
+        return laptopRepository.findById(id);
+    }
 
     public Laptop saveLaptop(Laptop laptop) {
         return laptopRepository.save(laptop);
     }
+
+    public Laptop updateLaptop(Laptop laptop) {
+        Laptop oldLaptop = laptopRepository.findById(laptop.getLaptopID()).orElseThrow();
+        return laptopRepository.save(laptop);
+    }
+
+    public void deleteByID(String id) {
+        laptopRepository.deleteById(id);
+    }
+
+    public void deleteAll() {
+        laptopRepository.deleteAll();
+    }
+
 }
